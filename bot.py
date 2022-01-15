af684951b5a1c906f4cf9f3a89498bf7d433cd3e
hiwilly66587 <hi.willy0302@gmail.com>
2022-01-15 22:06:47 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,30 @@
 import discord
 from discord.ext import commands
+import json
 
-bot = commands.Bot(command_prefix='g')
+with open('setting.json','r',encoding='utf8') as jfile:
+    jdata = json.load(jfile)
+
+bot = commands.Bot(command_prefix='')
 
 @bot.event
 async def on_ready():
     print(">> bot is online <<")
 
-bot.run("OTMxNzc0MTkxMDE4MDcwMDQ2.YeJUVg.VlS--679fURuRMyfvqUB3GsKWAc")
+@bot.command()
+async def 機器人延遲(ctx):
+    await ctx.send(f'{round(bot.latency*1000)}ms')
+
+@bot.command()
+async def 你的頭像(ctx):
+    pic = discord.File('C:\\Users\\hiwil\\Documents\\GitHub\\genshin_bot\\pic\\ga28kdyivbb71.jpg')
+    await ctx.send(file= pic)
+
+@bot.command()
+async def 胡桃(ctx):
+    if bot.get_channel(id) == bot.get_channel(931567610699599913):
+      channel = bot.get_channel(931567610699599913)
+      await channel.send(jdata['胡桃pic'])
+      await channel.send(jdata['胡桃info'])
+
+bot.run(jdata['TOKEN'])
